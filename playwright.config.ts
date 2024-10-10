ca1a93d02a2bbf605cac14df7ec30f2867e2e1a0
naopeke <naopeke@protonmail.com>
2024-10-10 12:03:00 +0200

    register

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,10 +39,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
   ],
   testMatch: ['tests/register.test.ts']
 });
--- tests/register.test.ts
+++ tests/register.test.ts
@@ -1,13 +1,17 @@
 import { test, expect } from '@playwright/test';
 
 test.beforeEach(async ({ page, baseURL })=>{
-    page.goto(`${baseURL}index.htm`)
+    await page.goto(`${baseURL}index.htm`)
 })
 
 test.describe ('Register', ()=>{
-    test('Register_01', async ({ page })=>{
+    test('Register_01', async ({ page, baseURL })=>{
         await page.getByRole('link', {name: 'Register'}).click();
-
+        //Received string: "https://parabank.parasoft.com/parabank/register.htm;jsessionid=7D55AB5EB44F76C622C2CAC412FC06F2"
+        // バックスラッシュは、特定の文字をエスケープ（無効化）するために使用されます。
+        // \/: スラッシュ（/）を正規表現の中で通常の文字として扱うためにエスケープします。
+        // \.: ドット（.）も特別な意味を持つため（任意の1文字を表す）、通常のドットとして扱いたい場合にエスケープします。
+        expect(page).toHaveURL(/\/register\.htm/);
     })
 
 
